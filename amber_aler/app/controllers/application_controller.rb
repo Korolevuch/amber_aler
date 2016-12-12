12f09c012660d781dca3458454689ad1eeaa8091
Korolevych Bogdan <Korolevych.bogdan@gmail.com>
2016-12-12 21:41:36 +0200

    add uk locale and avatar

--- amber_aler/app/controllers/application_controller.rb
+++ amber_aler/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
 =begin def authenticate_user!
@@ -19,4 +24,5 @@ class ApplicationController < ActionController::Base
       end
 =end
 end
+
 
