2acec9a1ac34124435b7f9d8f3f410b003a08aea
bohdan <korolevych.bogdan@gamil.com>
2017-01-03 18:51:22 +0200

    fix alignment

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -15,17 +15,16 @@ class Emergency < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
 
-  ACTIVE = 0
-  CLOSED = 1
+  ACTIVE   = 0
+  CLOSED   = 1
   ARCHIVED = 2
 
   enum status: { active: ACTIVE, closed: CLOSED, archived: ARCHIVED }
 
-  scope :not_archived, -> { where.not(status: ARCHIVED) }
+  scope :not_archived,      -> { where.not(status: ARCHIVED) }
   scope :not_archived_desc, -> { not_archived.order(updated_at: :desc) }
 
   def allow_messages_for?(current_user)
     active? || current_user == user
   end
-
 end
